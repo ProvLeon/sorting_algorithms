eafea1d610d02fdf6fe9220fe741e7dac086060e
ProvLeon <providence.leonard@gmail.com>
2023-04-26 12:42:24 +0100

    updated insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,27 @@
 #include "sort.h"
 
 /**
+ * insertion_sort_list - function to swap a doubly linked list
+ * @list: double pointer that points to the first node
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		insert_node(list, current);
+		current = current->next;
+		print_list(*list);
+	}
+}
+
+/**
  * insert_node - function to swap the nodes
  * @list: double pointer to hold the first node
  * @node: is a pointer to hold any other secondary node
@@ -22,26 +43,5 @@ void insert_node(listint_t **list, listint_t *node)
 		else
 			node->prev->next = node;
 		temp = node->prev;
-	}
-}
-
-/**
- * insertion_sort_list - function to swap a doubly linked list
- * @list: double pointer that points to the first node
- */
-void insertion_sort_list(listint_t **list)
-{
-	listint_t *current;
-
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-
-	current = (*list)->next;
-
-	while (current != NULL)
-	{
-		insert_node(list, current);
-		current = current->next;
-		print_list(*list);
 	}
 }
