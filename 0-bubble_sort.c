0df65f5b6f21a2860fa854de0fe46b0dd33e246b
ProvLeon <providence.leonard@gmail.com>
2023-04-26 11:15:07 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	int swapped, i, j, k;
+	for (i = 0; i < (int)size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < (int)size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1]) {
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				/* Print the current state of the array after each pass */
+				printf("After pass %d: ", i + 1);
+				for (k = 0; k < (int)size; k++)
+				{
+					printf("%d ", array[k]);
+				}
+				printf("\n");
+
+			}
+		}
+		/* If no two elements were swapped by inner loop, then break */
+		if (swapped == 0)
+			break;
+	}
+}
+
