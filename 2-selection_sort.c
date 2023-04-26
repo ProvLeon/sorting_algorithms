5ef4882b71506819553e74fe7ec5dc846cdf6b1d
ProvLeon <providence.leonard@gmail.com>
2023-04-26 14:02:29 +0100

    updated

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,6 +12,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
@@ -20,6 +21,7 @@ void selection_sort(int *array, size_t size)
 		if (min_idx != i)
 		{
 			int temp = array[i];
+
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
 			print_array(array, size);
